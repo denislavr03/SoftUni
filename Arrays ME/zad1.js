fc8861fe9a04b1c46c6217b9088bbfbc00e3f5cb
Denislav Raychev <147688797+denislavr03@users.noreply.github.com>
2024-05-14 01:22:55 +0300

    Delete Arrays ME directory

--- Arrays ME/zad1.js
+++
@@ -1,9 +0,0 @@
-function printNthEl(arr) {
-    let brr = [];
-    for (let index = 0; index < arr.length-1; index += Number(arr[arr.length - 1])) {
-        brr.push(arr[index]);
-    }
-    console.log(brr.join(' '));
-}
-
-printNthEl(['dsa', 'asd', 'test', 'test', '2']);
